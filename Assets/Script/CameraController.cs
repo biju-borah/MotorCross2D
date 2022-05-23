1d0d6e5d706ae4d75b87735ef73fc65bd9112695
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-05-23 23:40:08 +0530

    initial commit

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.z = -10f;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 1500;
+    public float rotationalSpeed = 1500f;
+    public WheelJoint2D backwheel;
+    private float movement = 0f;
+    private float rotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = -Input.GetAxisRaw("Vertical")*speed; // It will return 1 for upward key( or W) & -1 for downward key(or S) or otherwise 0
+        rotation = Input.GetAxisRaw("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        if (movement == 0f)
+        {
+            backwheel.useMotor = false;
+        }
+        else{
+            backwheel.useMotor = true;
+            JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = 10000};
+            backwheel.motor = motor;
+        }
+        
+        rb.AddTorque(-rotation * rotationalSpeed * Time.fixedDeltaTime);
+    }
+}
